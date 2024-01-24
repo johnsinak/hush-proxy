d6f06bccb15bd8ac57cb02a503378e743d22987d
sinku <sininoir@gmail.com>
2024-01-23 23:38:20 -0500

    Add new nat code

--- src/nat.py
+++ src/nat.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import requests
 
 class EchoThread(threading.Thread):
     def __init__(self, client_socket: socket.socket, client_address:str):
@@ -30,9 +31,43 @@ def echo_server(host, port):
         thr = EchoThread(client_socket, client_address)
         thr.start()
 
+class NATThread(threading.Thread):
+    def __init__(self, client_socket: socket.socket, client_address:str):
+        threading.Thread.__init__(self)
+        self.client_socket = client_socket
+        self.client_address = client_address
+
+    def run(self):
+        data = self.client_socket.recv(1024)
+        
+
+        while data:
+            url = data.decode()
+            response = requests.get(url)
+
+            self.client_socket.sendall(response.text.encode())
+            data = self.client_socket.recv(1024)
+        
+        print(f"Connection from {self.client_address} closed.")
+        self.client_socket.close()
+
+def nat_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+
+    print(f"Nat server is listening on {host}:{port}")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Accepted connection from {client_address}")
+
+        thr = NATThread(client_socket, client_address)
+        thr.start()
+
 
 if __name__ == "__main__":
     host = "0.0.0.0"
     port = 8000  
 
-    echo_server(host, port)
+    nat_server(host, port)
