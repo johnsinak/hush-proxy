3a707f5edb9ccc13a6c7b03ca0208d63d7db7359
sinku <sininoir@gmail.com>
2024-01-29 21:26:21 -0500

    Add beeg file test to nat

--- src/nat.py
+++ src/nat.py
@@ -1,86 +1,53 @@
 import socket
-import threading
 import requests
-from struct import pack
+import os
 
-class EchoThread(threading.Thread):
-    def __init__(self, client_socket: socket.socket, client_address:str):
-        threading.Thread.__init__(self)
-        self.client_socket = client_socket
-        self.client_address = client_address
+from nat_threads import *
 
-    def run(self):
-        data = self.client_socket.recv(1024)
-        while data:
-            self.client_socket.send(data)
-            data = self.client_socket.recv(1024)
-        
-        print(f"Connection from {self.client_address} closed.")
-        self.client_socket.close()
 
 def echo_server(host, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
+    nat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    nat_socket.bind((host, port))
+    nat_socket.listen(5)
 
     print(f"yes")
 
     while True:
-        client_socket, client_address = server_socket.accept()
+        client_socket, client_address = nat_socket.accept()
         print(f"Accepted connection from {client_address}")
 
         thr = EchoThread(client_socket, client_address)
         thr.start()
 
-class NATThread(threading.Thread):
-    def __init__(self, client_socket: socket.socket, client_address:str):
-        threading.Thread.__init__(self)
-        self.client_socket = client_socket
-        self.client_address = client_address
-
-    def run(self):
-        data = self.client_socket.recv(1024)
-        while data:
-            url = data.decode()
-            response = requests.get(url)
-            message = response.text.encode()
-            length = pack('>Q', len(message))
-
-            self.client_socket.sendall(length)
-            self.client_socket.sendall(message)
-
-            data = self.client_socket.recv(1024)
-        self.client_socket.close()
 
 def nat_server(host, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
+    nat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    nat_socket.bind((host, port))
+    nat_socket.listen(5)
 
     print(f"yes")
 
     while True:
-        client_socket, client_address = server_socket.accept()
+        client_socket, client_address = nat_socket.accept()
         print(f"Accepted connection from {client_address}")
 
         thr = NATThread(client_socket, client_address)
         thr.start()
 
 
-def get_public_ip():
-    try:
-        response = requests.get('https://httpbin.org/ip')
+def nat_server_with_bulk_downloads(host, port, beeg_file_path):
+    nat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    nat_socket.bind((host, port))
+    nat_socket.listen(5)
 
-        if response.status_code == 200:
-            public_ip = response.json()['origin']
-            return public_ip
-        else:
-            print(f"Failed to retrieve public IP. Status code: {response.status_code}")
+    print('going beeg')
 
-    except requests.RequestException as e:
-        print(f"Request error: {e}")
+    while True:
+        client_socket, client_address = nat_socket.accept()
+        print(f"Accepted connection from {client_address}")
 
-    return None
+        thr = BEEGThread(client_socket, client_address)
+        thr.start()
 
 
 if __name__ == "__main__":
